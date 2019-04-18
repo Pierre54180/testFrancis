d47fb158e6c0f73b9db6a3e17a9778091989975e
Pierre DELCROS <pdelcros@CAUDALIE.LAN>
2019-04-18 11:57:45 +0200

    df

--- Application/Personnage/Mage/mage.php
+++ Application/Personnage/Mage/mage.php
@@ -1,9 +1,7 @@
 <?php
 
-<<<<<<< HEAD
-echo "Francis le mage jeffectue ma modification avec mon visual studio";
-=======
-echo "Francis le mage jeffectue ma modification avec gitHubhihi";
->>>>>>> a14dbc38f6562888a07c42a6eb329774c611bea8
+
+echo "Francis le mage jeffectue ma modification avec gitHubhihivisual";
+
 
 ?>
